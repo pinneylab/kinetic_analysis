104404c774bcf2b11f84c625151985e4c2d58f5e
jzhang1198 <jzhang1198@gmail.com>
2024-11-25 13:33:45 -0800

    Dependencies now in requirements.txt file

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup, find_packages
+import os
+
+# parse requirements.txt to find dependencies
+library_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"{library_folder}/requirements.txt"
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name='kinetic-analysis',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scipy',
-        'matplotlib',
-        'scikit-learn'
-    ],
+    install_requires=install_requires,
     author= ['Duncan Muir', 'Nicholas Freitas'],
     author_email="duncan.muir@ucsf.edu",
 #    description='what does this do?',
